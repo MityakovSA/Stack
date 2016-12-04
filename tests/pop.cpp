7bb3923b50e353fcc33da39df110a7b3341f0dcc
MityakovSA <mitykovsergey@mail.ru>
2016-12-04 22:30:57 +0300

    I stage (with working tests)

--- tests/pop.cpp
+++ tests/pop.cpp
@@ -14,7 +14,7 @@ SCENARIO("pop() must delete the top element of the stack and return its value")
         {
             THEN("pop() must delete value 2 from the top and return it")
             {
-                REQUIRE((s.pop() == 2) && (s.count() == 2) && (s.pop == 1));
+                REQUIRE( ((s.pop() == 2) && (s.count() == 2) && (s.pop() == 1)) == true );
             }
         }
     }
--- tests/push.cpp
+++ tests/push.cpp
@@ -12,7 +12,7 @@ SCENARIO("push() must add value to the top of the stack")
             s.push(0);
             THEN("push() must allocate memory for 5 elements and add value 0 to the top of the stack")
             {
-                REQUIRE((s.size() == 5) && (s.count() == 1) && (s.pop() == 0));
+                REQUIRE( ((s.size() == 5) && (s.count() == 1) && (s.pop() == 0)) == true );
             }
         }
     }
@@ -26,7 +26,7 @@ SCENARIO("push() must add value to the top of the stack")
             s.push(2);
             THEN("Value 2 must be at the top of the stack and number of elements must be 3")
             {
-                REQUIRE((s.count() == 3) && (s.pop() == 2));
+                REQUIRE( ((s.count() == 3) && (s.pop() == 2)) == true );
             }
         }
     }
@@ -44,7 +44,7 @@ SCENARIO("push() must add value to the top of the stack")
             s.push(5);
             THEN("push() must reallocate memory by doubling previous and add value 5 to the top of the stack")
             {
-                REQUIRE((s.size() == (size*2)) && (s.count() == 6) && (s.pop() == 5));
+                REQUIRE( ((s.size() == (size*2)) && (s.count() == 6) && (s.pop() == 5)) == true );
             }
         }
     }
