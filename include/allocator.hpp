ccfe306f395cc982f4af3eeffd5e1e626494fd6a
MityakovSA <mitykovsergey@mail.ru>
2016-12-14 22:15:40 +0300

    Stage III

---
+++ include/allocator.hpp
@@ -0,0 +1,74 @@
+#include <memory>
+#include <cstddef>
+#include <stdexcept>
+#include <algorithm>
+
+
+template <typename T>
+class allocator
+{
+protected:
+    allocator(size_t size = 0) : size_(size), count_(0), ptr_( static_cast<T*>(::operator new(size * sizeof(T))) ) {}
+    allocator(allocator const&) = delete;
+    ~allocator();
+
+    auto operator =(allocator const&) -> allocator& = delete;
+
+    auto swap(allocator& other) -> void;
+    auto allocate() -> void;
+    auto reallocate() -> void;
+
+    size_t size_;
+    size_t count_;
+    T* ptr_;
+};
+
+
+template <typename T>
+allocator<T>::~allocator()
+{
+    for (size_t i = 0; i < count_; ++i)
+        ptr_[i].~T();
+    ::operator delete[](ptr_);
+}
+
+
+template <typename T>
+auto allocator<T>::swap(allocator &other) -> void
+{
+    std::swap(size_, other.size_);
+    std::swap(count_, other.count_);
+    std::swap(ptr_, other.ptr_);
+}
+
+
+template <typename T>
+auto allocator<T>::allocate() -> void
+{
+    if (count_ == size_)
+    {
+        size_ *= 2;
+
+        try
+        {
+            reallocate();
+        }
+        catch (...)
+        {
+            size_ /= 2;
+            throw;
+        }
+
+    }
+    return;
+}
+
+
+template <typename T>
+auto allocator<T>::reallocate() -> void
+{
+    allocator<T> alloc(size_);
+    std::copy(ptr_, ptr_ + count_, alloc.ptr_);
+    alloc.count_ = count_;
+    swap(alloc);
+}
--- include/stack.hpp
+++ include/stack.hpp
@@ -1,92 +1,75 @@
-#include <cstddef>
-#include <stdexcept>
-#include <algorithm>
+#include <allocator.hpp>
 
 
 template <typename T>
-class stack
+class stack : public allocator<T>
 {
 public:
-    stack() : array_(new T [init_size_]), array_size_(init_size_), count_(0) {}
+    stack() : allocator<T>(init_size_) {}
+
     auto size() const noexcept -> size_t;
     auto count() const noexcept -> size_t;
     auto empty() const noexcept -> bool;
     auto push(const T&) /* strong */ -> void;
     auto pop() noexcept -> void;
     auto top() const /* strong */ -> T;
-    ~stack() { delete[] array_; }
+
 private:
     static const size_t init_size_ = 5;
-    T* array_;
-    size_t array_size_;
-    size_t count_;
 };
 
 
 template <typename T>
 auto stack<T>::size() const noexcept -> size_t
 {
-    return array_size_;
+    return allocator<T>::size_;
 }
 
 
 template <typename T>
 auto stack<T>::count() const noexcept -> size_t
 {
-    return count_;
+    return allocator<T>::count_;
 }
 
 
 template <typename T>
 auto stack<T>::empty() const noexcept -> bool
 {
-    return (count_ ? false : true);
+    return (allocator<T>::count_ ? false : true);
 }
 
 
 template <typename T>
 auto stack<T>::push(const T& value) /* strong */ -> void
 {
-    T* backup = array_;
-    T* new_array = nullptr;
     bool realloc = false;
-    if (count_ == array_size_)
+
+    try
     {
-        array_size_ *= 2;
-
-        try
-        {
-            new_array = new T[array_size_];
-            std::copy(array_, array_+count_, new_array);
-        }
-        catch (...)
-        {
-            delete[] new_array;
-            array_size_ /= 2;
-            throw;
-        }
-
-        array_ = new_array;
+        allocator<T>::allocate();
         realloc = true;
+    }
+    catch (...)
+    {
+        throw;
     }
 
     try
     {
-        array_[count_] = value;
+        allocator<T>::ptr_[allocator<T>::count_] = value;
     }
     catch (...)
     {
         if (realloc)
         {
-            delete[] array_;
-            array_ = backup;
-            array_size_ /= 2;
+            allocator<T>::size_ /= 2;
+            allocator<T>::reallocate();
         }
         throw;
     }
 
-    ++count_;
-    if (realloc) delete[] backup;
+    ++allocator<T>::count_;
     return;
 }
 
@@ -94,8 +77,8 @@ auto stack<T>::push(const T& value) /* strong */ -> void
 template <typename T>
 auto stack<T>::pop() noexcept -> void
 {
-    if (count_)
-        --count_;
+    if (allocator<T>::count_)
+        --allocator<T>::count_;
     return;
 }
 
@@ -103,9 +86,9 @@ auto stack<T>::pop() noexcept -> void
 template <typename T>
 auto stack<T>::top() const /* strong */ -> T
 {
-    if (count_)
+    if (allocator<T>::count_)
     {
-        T value = array_[count_ - 1];
+        T value = allocator<T>::ptr_[allocator<T>::count_ - 1];
         return value;
     }
     throw std::underflow_error("Stack is empty!");
